5edb6de967509e5c92e1e9e2de021f39a6b3fb86
Yulia Milovanova <ymilovanova@actionlearning.ru>
2021-01-20 11:41:49 +0300

    Initial Commit

---
+++ selfSizingLabel3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  selfSizingLabel3
+//
+//  Created by Preacher on 20.01.2021.
+//  Copyright © 2021 Yulia Milovanova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
